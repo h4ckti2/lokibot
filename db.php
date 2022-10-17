2e199d55010f36608593fe71f098cd24f701fcad
hackticubrube <109615015+hackticubrube@users.noreply.github.com>
2022-10-17 09:35:33 +0300

    loki

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$db_host = "127.0.0.1";
+$db_name = "loki";
+$db_user = "lokiadmin";
+$db_pass = "Ajh95a6M";
+
+define('cryptKey' , 'loki');
+$db_table_to_show = 'bot_commerz2';
+$mysqli = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+?>
